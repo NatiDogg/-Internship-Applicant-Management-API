a8bbd14dc3bac98075d5730accdc65bd95b9054e
natiDogg <natnael.wondmu@gmail.com>
2026-07-22 12:49:46 -0700

    created and done with my createApplicant service method

--- src/applicant/applicant.service.ts
+++ src/applicant/applicant.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateApplicantDto } from './dtos/createApplicantDto';
+import { Prisma } from 'prisma/generated/prisma/client';
 
 @Injectable()
-export class ApplicantService {}
+export class ApplicantService {
+
+    constructor(private prisma:PrismaService){}
+
+    async createApplicant(applicantDetails:CreateApplicantDto){
+           const normalizedEmail = applicantDetails.email.toLowerCase()
+        try {
+            const newlyCreatedApplicant = await this.prisma.applicant.create({data:{
+                
+                ...applicantDetails,
+                email: normalizedEmail,
+              
+                
+
+            }})
+            return {
+                success: true,
+                message: 'Applicant Created Successfully',
+                data: newlyCreatedApplicant
+            }
+        } catch (error) {
+            if(error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002'){
+                 throw new BadRequestException("An applicant with this email already exists")
+            }
+            throw error
+        }
+
+
+
+    }
+}
